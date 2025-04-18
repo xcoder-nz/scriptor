f22c97e5daca52ca06abc0e1279ed365ff40d413
kkobtk <kkobtk@gmail.com>
2025-04-18 08:00:04 +0000

    Examples

---
+++ examples/Hello.java
@@ -0,0 +1,14 @@
+// Hello.java
+
+public class Hello {
+    /**
+     * Prints a greeting message.
+     */
+    public static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+
+    public static void main(String[] args) {
+        greet("World");
+    }
+}
---
+++ examples/bar.js
@@ -0,0 +1,11 @@
+// bar.js
+
+/**
+ * Multiply two numbers.
+ * @param {number} x
+ * @param {number} y
+ * @returns {number}
+ */
+const multiply = (x, y) => x * y;
+
+console.log(multiply(4, 7));
